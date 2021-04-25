ce0a507b15b9f84212fb0e9fa9bcc2e970d6d5fb
Vadim Kirzhakov <kirzhakov.2012@mail.ru>
2021-04-26 01:23:48 +0300

    fix components style

---
+++ src/components/cities/cities.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './cities.css'
+
+const Cities = () => {
+	return (
+		<div className='block_one'>
+			<div>
+				<h1>Cities:</h1>
+			</div>
+			<div>list</div>
+			<div>
+				<div>
+					<textarea></textarea>
+				</div>
+				<div>
+					<butonn class='btn btn-info'>Add cities</butonn>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Cities
---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './forecast.css'
+
+const Forecast = () => {
+	return (
+		<div className='block_two'>
+			<div>
+				<h1>Forecast:</h1>
+			</div>
+		</div>
+	)
+}
+
+export default Forecast
